5aa01650db2b44b4d9ebded58ae8bfcb4a3be3b1
Huzajfha Bayoumi <hubay22@student.sdu.dk>
2025-05-17 15:11:43 +0200

    provides added to player module-info
     playerPlugin class added as well

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -1,3 +1,8 @@
+import dk.sdu.cbse.services.IEntityProcessingService;
+import dk.sdu.cbse.services.IGamePluginService;
+
 module Player {
     requires Common;
+    provides IEntityProcessingService with dk.sdu.cbse.PlayerControlSystem;
+    provides IGamePluginService with dk.sdu.cbse.PlayerPlugin;
 }
