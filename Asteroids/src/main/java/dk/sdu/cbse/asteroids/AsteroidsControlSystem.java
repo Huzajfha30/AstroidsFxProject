fbc6dc703d0154aed7fd993fff95d1bd7fdfae06
Huzajfha Bayoumi <hubay22@student.sdu.dk>
2025-05-19 16:25:28 +0200

    asteroids module added to the 2d asteroidgame and tested as well in addition tryng to make a commonAsteroid to make sure that we can make a common to asteroid seperatly as well

---
+++ Asteroids/src/main/java/dk/sdu/cbse/asteroids/AsteroidsControlSystem.java
@@ -0,0 +1,37 @@
+package dk.sdu.cbse.asteroids;
+
+import dk.sdu.cbse.common.asteroids.Asteroids;
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IEntityProcessingService;
+
+public class AsteroidsControlSystem implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity asteroid : world.getEntities(Asteroids.class)) {
+            double changeX = Math.cos(Math.toRadians(asteroid.getRotation()));
+            double changeY = Math.sin(Math.toRadians(asteroid.getRotation()));
+
+            asteroid.setX(asteroid.getX() + changeX * 0.5);
+            asteroid.setY(asteroid.getY() + changeY * 0.5);
+
+            if (asteroid.getX() < 0) {
+                asteroid.setX(asteroid.getX() - gameData.getDisplayWidth());
+            }
+
+            if (asteroid.getX() > gameData.getDisplayWidth()) {
+                asteroid.setX(asteroid.getX() % gameData.getDisplayWidth());
+            }
+
+            if (asteroid.getY() < 0) {
+                asteroid.setY(asteroid.getY() - gameData.getDisplayHeight());
+            }
+
+            if (asteroid.getY() > gameData.getDisplayHeight()) {
+                asteroid.setY(asteroid.getY() % gameData.getDisplayHeight());
+            }
+
+        }
+    }
+}
